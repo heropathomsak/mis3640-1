225b46a6bcda203a64860287e597a2db638d3e97
Zhi Li <lzblack@users.noreply.github.com>
2020-09-01 21:55:59 -0400

    Finish session 03 class

---
+++ session03/demo.py
@@ -0,0 +1,39 @@
+# print('Hello, world!')
+
+# print('Hey Jude', 'don\'t make it bad', 'Take a sad song')
+# \ escape character
+# print("Hey Jude", "don't make it bad", "Take a sad song")
+
+# print("The total number of overall medals in Rio 2016 is", 46 + 37 + 38)
+
+# name = input("Say something: ")
+
+# print(name)
+
+# message = 'I did something cool today!'
+
+# print(message)
+
+# print('message')
+
+first_name = 'John'
+last_name = 'Lennon'
+full_name = first_name + " " + last_name
+
+# print('Hello,', full_name)
+
+# print('Naah, na na nanana naah, nanana naah, hey Jude.\n' * 10)
+
+# print(f'Hello, {full_name}.')
+
+# pi = 3.1415926
+# another_number = 20.2
+
+# print(f'Pi equals {pi}.')
+# print(f'Pi equals {pi:.5f}.')
+# print(f'Pi equals {pi:8.5f}.')
+# print(f'An equals {another_number:8.5f}.')
+
+
+# TODO: try all the examples in notebook
+
---
+++ session03/hello.py
@@ -0,0 +1,9 @@
+name = input('What is your name >> ')
+yob = input('Which year were you born >> ')
+
+# print(type(yob))
+
+age = 2020 - int(yob)
+
+print('Hello,', name)
+print('Your age is', age)
