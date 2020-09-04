207fcb25509ba96312bc5fc12aa5ebf1ac3826ab
Zhi Li <lzblack@users.noreply.github.com>
2020-09-03 22:27:22 -0400

    Create demo.py

---
+++ session04/demo.py
@@ -0,0 +1,19 @@
+# print('''line1
+# line2
+# line3'''
+# )
+
+""" 
+This is a comment
+This is another comment
+This is another comment
+ """
+
+
+age = input('Please enter you age >> ')
+age = int(age)
+
+if age >= 21:
+    print('Yes, you can.')
+else:
+    print('No, you cannot.')
