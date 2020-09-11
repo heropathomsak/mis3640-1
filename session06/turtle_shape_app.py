1a2f6c39241996f782ff42996f0f8dd5cbd4917c
Zhi Li <lzblack@users.noreply.github.com>
2020-09-11 11:28:33 -0400

    Create turtle_shape_app.py

---
+++ session06/turtle_shape_app.py
@@ -0,0 +1,15 @@
+import turtle_shape
+import turtle
+
+leo = turtle.Turtle()
+
+# draw a square at the defaul origin position
+turtle_shape.square(leo, 100)
+
+# move pen to a new position (200px to eastward, 100px northward)
+turtle_shape.move(leo, 200, 100)
+
+# draw a circle at the new position
+turtle_shape.circle(leo, 50)
+
+turtle.mainloop()
