7790b4c376b43648da99713bcff5f8d967bc20c0
Zhi Li <lzblack@users.noreply.github.com>
2020-09-24 15:53:57 -0400

    Create guess.py

---
+++ session09/guess.py
@@ -0,0 +1,47 @@
+import random
+
+
+def guess(max_number=20, total_tries=6):
+    """
+    A "Guess the Number" game.
+    The program will think of a random number from 1 to max_number (default 20), and ask user to guess it.
+    The program will tell user if each guess is too high or too low.
+    The user wins if user can guess the number within total_tries (default 6) tries.
+    """
+    guesses_taken = 0
+    print('Hello! What is your name?')
+    name = input('>> ')
+
+    number = random.randint(1, max_number)
+    print(f'Well, {name}, I am thinking of a number between 1 and {max_number}.')
+
+    while guesses_taken < total_tries:
+        print('Take a guess:')
+        guess = input('>> ')
+        while not guess.isdigit():
+            print('Please enter an integer:')
+            guess = input('>> ')
+        guess = int(guess)
+
+        guesses_taken += 1
+        if guess < number:
+            print('Your guess is too low.')
+        elif guess > number:
+            print('Your guess is too high.')
+        else:
+            print(
+                f'Good job, {name} ! You guessed my number in {guesses_taken} guesses!'
+            )
+            break
+
+    if guess != number:
+        print(f'Nope. The number I was thinking was {number}.')
+
+
+def main():
+    # guess(10, 4)
+    guess()
+
+
+if __name__ == "__main__":
+    main()
