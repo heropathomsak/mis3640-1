9d36797760bb913d9c83b19a05da8e1f92decf4c
Zhi Li <lzblack@users.noreply.github.com>
2020-10-08 21:46:23 -0400

    Update people_in_space.py

--- session13/people_in_space.py
+++ session13/people_in_space.py
@@ -5,11 +5,18 @@ url = "http://api.open-notify.org/astros.json"
 
 with urllib.request.urlopen(url) as f:
     response_text = f.read().decode('utf-8')
+    # print(response_text)
     j = json.loads(response_text)
     print(j)
 
 # print(len(j))
 # Can you print number of people in the space?
+# print(j['number'])
 
 
 # Can you print all the names?
+# print(j['people'])
+
+people = j['people']
+for person in people:
+    print(person['name'])
