ce3fff3eb07459b8017807570ec93e0c4d24d36c
Zhi Li <lzblack@users.noreply.github.com>
2020-10-08 21:46:29 -0400

    Create anagrams_2.py

---
+++ session14/anagrams_2.py
@@ -0,0 +1,47 @@
+"""
+1. read the file, words.txt, save the words into a list
+
+2. sort every word
+'cameron' -> 'acemnor' ('a', 'c'...) (signature of the word)
+'romance' -> 'acemnor'
+
+3. create a dictionary, key is the signature,
+value is a list of words that have the same signature
+
+{'acemnor': ['cameron', 'romance'], 'eiknv': ['kevin'], ...}
+
+4. get values only, only those lists with more than one word
+
+5. print the lists
+
+
+6. create a dictionary like this:
+
+{6: ['deltas', 'desalt', 'lasted', 'salted', 'slated', 'staled'],
+
+3: ['resmelts', 'smelters', 'termless'],
+
+2: [['retainers', 'ternaries'], ['generating', 'greatening']] }
+
+"""
+
+
+def file_to_list(filename):
+    """returns a list """
+
+def sort_word(word):
+    """returns a new sorted string, or a tuple"""
+
+
+def list_to_dict(wordlist):
+    """
+    return a dictionry like 
+    
+{'acemnor': ['cameron', 'romance'], 'eiknv': ['kevin'], ...}
+
+    this function will use sort_word
+    """
+
+
+def main():
+    """"""
